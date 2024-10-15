4d42ebeef75d627d5fac2e2bc3916d156da2cfb8
Brian Costa <67477751+BrianCosta2004@users.noreply.github.com>
2024-10-14 22:45:57 -0300

    Add files via upload

--- Vitapina/main.py
+++ Vitapina/main.py
@@ -3,19 +3,26 @@ from kivy.lang import Builder
 from telas import *
 from botoes import *
 import requests
-from bannervenda import BannerVenda
 import os
 from functools import partial
 from myfirebase import MyFirebase
-from bannervendedor import BannerVendedor
 from datetime import date
 
 GUI = Builder.load_file("main.kv")
 
 
 class MainApp(App):
+    cliente = None
+    produto = None
+    unidade = None
 
     def build(self):
+        self.firebase = MyFirebase()
         return GUI
+
+
+    def mudar_tela(self, id_tela):
+        gerenciador = self.root.ids["screen_manager"]
+        gerenciador.current = id_tela
 
 MainApp().run()
