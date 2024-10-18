41b75b7b932ef7a111027925c1a54d79cf820810
brian <brianhcosta@gmail.com>
2024-10-18 20:23:48 -0300

    Conexão com Banco de Dados - Adicionar a funcionalidade dos botões de navegação e Login/Cadastro.

--- Vitapina/myfirebase.py
+++ Vitapina/myfirebase.py
@@ -2,9 +2,9 @@ import requests
 from kivy.app import App
 
 class MyFirebase():
-    API_KEY = "AIzaSyA1pB5EbM-WZohJhjXkpe2OVYeKUrRoFdE"
+    API_KEY = "AIzaSyC-Xb5RIUljluE8_KI2W5kxWLT_eXZRnpk"
 
-    def criar_conta(self, email, senha):
+    def criar_conta(self, email, senha, nome, sobrenome, telefone):
         link = f"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={self.API_KEY}"
 
         info = {"email": email,
@@ -26,26 +26,25 @@ class MyFirebase():
             with open("refreshtoken.txt", "w") as arquivo:
                 arquivo.write(refresh_token)
 
-            req_id = requests.get("https://appcontrole-4f122-default-rtdb.firebaseio.com/proximo_id.json")
+            req_id = requests.get("https://vitapinabd-default-rtdb.firebaseio.com//proximo_id.json")
             id_vendedor = req_id.json()
 
-            link = f"https://appcontrole-4f122-default-rtdb.firebaseio.com/{local_id}.json"
-            info_usuario = f'{{"foto": "foto1.png", "equipe": "", "total_vendas": "0", "vendas": "", "id_vendedor": "{id_vendedor}"}}'
+            link = f"https://vitapinabd-default-rtdb.firebaseio.com//{local_id}.json"
+            info_usuario = f'{{"Nome": "{nome}", "Sobrenome": "{sobrenome}", "telefone": "{telefone}","E-mail": "{email}"}}'
             requisicao_usuario = requests.patch(link, data=info_usuario)
 
             proximo_id = int(id_vendedor) + 1
             info_id = f'{{"proximo_id": "{proximo_id}"}}'
-            requests.patch("https://appcontrole-4f122-default-rtdb.firebaseio.com/.json", data=info_id)
+            requests.patch("https://vitapinabd-default-rtdb.firebaseio.com//.json", data=info_id)
 
-            meu_aplicativo.carregar_infos_usuario()
-            meu_aplicativo.mudar_tela("homepage")
+            meu_aplicativo.mudar_tela("glicemiapage")
 
         else:
             mensagem_erro = requisicao_dic["error"]["message"]
             meu_aplicativo = App.get_running_app()
-            pagina_login = meu_aplicativo.root.ids["loginpage"]
-            pagina_login.ids["mensagem_login"].text = mensagem_erro
-            pagina_login.ids["mensagem_login"].color = (1, 0, 0, 1)
+            pagina_login = meu_aplicativo.root.ids["cadastropage"]
+            pagina_login.ids["mensagem_erro"].text = mensagem_erro
+            pagina_login.ids["mensagem_erro"].color = (1, 0, 0, 1)
 
     def fazer_login(self, email, senha):
         link = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={self.API_KEY}"
@@ -69,15 +68,14 @@ class MyFirebase():
             with open("refreshtoken.txt", "w") as arquivo:
                 arquivo.write(refresh_token)
 
-            meu_aplicativo.carregar_infos_usuario()
-            meu_aplicativo.mudar_tela("homepage")
+            meu_aplicativo.mudar_tela("glicemiapage")
 
         else:
             mensagem_erro = requisicao_dic["error"]["message"]
             meu_aplicativo = App.get_running_app()
             pagina_login = meu_aplicativo.root.ids["loginpage"]
-            pagina_login.ids["mensagem_login"].text = mensagem_erro
-            pagina_login.ids["mensagem_login"].color = (1, 0, 0, 1)
+            pagina_login.ids["mensagem_erro"].text = mensagem_erro
+            pagina_login.ids["mensagem_erro"].color = (1, 0, 0, 1)
 
     def trocar_token(self, refresh_token):
         link = f"https://securetoken.googleapis.com/v1/token?key={self.API_KEY}"
