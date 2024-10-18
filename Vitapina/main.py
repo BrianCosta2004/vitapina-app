25e67dd259ec316b64cbbc4ea5b309ba766558cd
brian <brianhcosta@gmail.com>
2024-10-17 23:00:00 -0300

    ScrollViews pagina de Receitas

--- Vitapina/main.py
+++ Vitapina/main.py
@@ -20,6 +20,33 @@ class MainApp(App):
         self.firebase = MyFirebase()
         return GUI
 
+    def on_start(self):
+        arquivos = os.listdir("icones/fotos_alimentos_cafe")
+        pagina_receitas = self.root.ids["receitaspage"]
+        lista_produtos = pagina_receitas.ids["lista_alimentos_cafe"]
+        for foto_produto in arquivos:
+            imagem = ImageButton(source=f"icones/fotos_alimentos_cafe/{foto_produto}")
+            label = LabelButton(text=foto_produto.replace(".png", "").capitalize())
+            lista_produtos.add_widget(imagem)
+            lista_produtos.add_widget(label)
+
+        arquivos = os.listdir("icones/fotos_alimentos_almoco")
+        pagina_receitas = self.root.ids["receitaspage"]
+        lista_produtos = pagina_receitas.ids["lista_alimentos_almoco"]
+        for foto_produto in arquivos:
+            imagem = ImageButton(source=f"icones/fotos_alimentos_almoco/{foto_produto}")
+            label = LabelButton(text=foto_produto.replace(".png", "").capitalize())
+            lista_produtos.add_widget(imagem)
+            lista_produtos.add_widget(label)
+
+        arquivos = os.listdir("icones/fotos_alimentos_janta")
+        pagina_receitas = self.root.ids["receitaspage"]
+        lista_produtos = pagina_receitas.ids["lista_alimentos_janta"]
+        for foto_produto in arquivos:
+            imagem = ImageButton(source=f"icones/fotos_alimentos_janta/{foto_produto}")
+            label = LabelButton(text=foto_produto.replace(".png", "").capitalize())
+            lista_produtos.add_widget(imagem)
+            lista_produtos.add_widget(label)
 
     def mudar_tela(self, id_tela):
         gerenciador = self.root.ids["screen_manager"]
